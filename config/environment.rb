82478234d64b5464e8450a92a895c90e3d664cd3
Saverio <saver.mucci@gmail.com>
2010-06-16 23:25:46 +0200

    missing gem

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'mislav-will_paginate', :version => '~> 2.3.8', :lib => 'will_paginate', 
     :source => 'http://gems.github.com'
-
+  config.gem 'fastercsv'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
